b01be9353bd7a401ba6827228d5516afb680b9c2
NEOGIGIO <gigio.forastieri@gmail.com>
2019-06-01 18:42:46 -0300

    changed App from class to function (react hooks)

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Title } from "./styles";
 
-class App extends Component {
-  state = {
-    response: ""
-  };
+export default function App() {
+  const [response, setResponse] = useState("");
 
-  componentDidMount() {
-    console.log("aaa");
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
+  async function callApi() {
     const response = await fetch("http://localhost:8000/api/mensagem");
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-    console.log(body);
-    return body;
-  };
-
-  render() {
-    return (
-      <>
-        <Title>Mds {this.state.response}</Title>
-      </>
-    );
+    console.log(body.express);
+    return String(body.express);
   }
-}
 
-export default App;
+  return (
+    <>
+      <Title>Mds {response}</Title>
+      <button
+        onClick={() => {
+          callApi().then(res => setResponse(res));
+        }}
+      >
+        Click me
+      </button>
+    </>
+  );
+}
